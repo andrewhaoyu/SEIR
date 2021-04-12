27981516c03749c2634e164b66f2ce58b28cd843
Zhang <andrew.haoyu@gmail.com>
2021-04-12 17:31:13 -0400

    udpate

--- scripts_main/run_SEIR_US_analysis.R
+++ scripts_main/run_SEIR_US_analysis.R
@@ -258,7 +258,7 @@ for(i in 1:(n.stage-1)){
 Dq[length(Dq)] = 3
 
 
-r0_vec = c(0.05,0.075,0.10,0.125,0.15,0.20)
+r0_vec = c(0.05,0.075,0.10,0.125,0.15,0.20,0.225,0.25,0.275,0.30)
 r0 = r0_vec[i3]
 init_sets_list=get_init_sets_list(r0=r0,
                                   Di = Di,
